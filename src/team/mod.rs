88bf8bea0ee982443d936c56e5b3846a0ed13551
unnamedgirl <unnamedgirl.public@gmail.com>
2025-06-18 23:22:41 +0800

    Introduce team deserialisation and feed parsing

---
+++ src/team/mod.rs
@@ -0,0 +1,71 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::{enums::{GameStat, MaybeRecognized, Position, PositionType, RecordType, Slot}, feed_event::FeedEvent};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "PascalCase")]
+pub struct Team {
+    // Cashews id
+    #[serde(rename = "_id")]
+    _id: Option<String>,
+    pub abbreviation: String,
+    pub active: bool,
+    pub augments: u16,
+    pub championships: u8,
+    pub color: String,
+    pub emoji: String,
+
+    #[serde(default)] // Teams deleted before season 1 won't have feeds,
+    pub feed: Vec<FeedEvent>,
+    pub motes_used: Option<u8>,
+
+    pub location: String,
+    pub full_location: String,
+    pub league: String,
+
+    /// no modifications have been seen, so left as raw json
+    pub modifications: Vec<Value>,
+    pub name: String,
+
+    // / no mottos have been seen, so left as raw json
+    pub motto: Option<serde_json::Value>,
+
+    #[serde(rename = "OwnerID")]
+    pub owner_id: Option<String>,
+
+    pub players: Vec<TeamPlayer>,
+    pub record: HashMap<MaybeRecognized<RecordType>, TeamRecord>,
+    pub season_records: HashMap<String, String>,
+
+    #[serde(flatten)]
+    pub extra_fields: serde_json::Map<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct TeamRecord {
+    pub losses: u16,
+    pub run_differential: i32,
+    pub wins: u16
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct TeamPlayer {
+    pub emoji: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub number: u8,
+    #[serde(rename = "PlayerID")]
+    pub player_id: String,
+    pub position: MaybeRecognized<Position>,
+    pub slot: MaybeRecognized<Slot>,
+    pub position_type: MaybeRecognized<PositionType>,
+    pub stats: HashMap<MaybeRecognized<GameStat>, i32>,
+
+    #[serde(flatten)]
+    pub extra_fields: serde_json::Map<String, serde_json::Value>,
+}
